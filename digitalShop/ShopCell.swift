ba27ec712a7ae7425a4b9c2a1cef088d94d8b6ae
Andrii Halabuda <losAngelesBoy11@gmail.com>
2017-06-08 13:37:53 +0300

    Added files & TableView settings

---
+++ digitalShop/ShopCell.swift
@@ -0,0 +1,23 @@
+//
+//  ShopCell.swift
+//  digitalShop
+//
+//  Created by Andrew Foster on 6/8/17.
+//  Copyright © 2017 Andrii Halabuda. All rights reserved.
+//
+
+import UIKit
+
+class ShopCell: UITableViewCell {
+
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
+    @IBOutlet weak var productImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+
+}
